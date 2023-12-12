a5e68bb28fcd7fa788fbc90344f352578891a0ba
Monday_David_Sarka <davidsarka242@gmail.com>
2023-12-12 01:07:30 +0100

    Added function with type keyof property

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  interface Person {
+    name: string;
+    age: number;
+    sex: string;
+  }
+  // `keyof Person` here creates a union type of "name" and "age", other strings will not be allowed
+  function printPersonProperty(person: Person, property: keyof Person) {
+    console.log(`Printing person property ${property}: "${person[property]}"`);
+  }
+  
+  let person = {
+    name: "Max",
+    age: 27,
+    sex: "Male"
+  };
+
+  printPersonProperty(person, "age");
+  printPersonProperty(person, "sex");
+
+//  useEffect(() => {  
+//   class Person {
+//     name: string;
+//   }
+//   const david = new Person
+//    console.log(david.name = "Sarka");
+//    console.log("Shiuut");
+//  }, []);
+
   return (
     <div className="App">
-     <h2>Simple Typescript React Tutorial on W3schools</h2>
+     <h2>Simple Typescript React Tutorial From W3schools</h2>     
     </div>
   );
 }
