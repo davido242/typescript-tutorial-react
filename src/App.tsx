561b6b67dccd89787fcd2c69d7ddc3a2cae80009
Monday_David_Sarka <davidsarka242@gmail.com>
2023-12-13 01:04:02 +0100

    Return and non-return function differences

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 function App() {
-              
-  function getSalue(): string | undefined {
-    return 'hello';
-  }
-              
-  let value = getSalue();
-  console.log('value length: ' + value?.length);
+  function greet(name : string) {
+    return ("Hello, " + name + "!");
+    // No explicit return statement
+}
 
-//  useEffect(() => {  
-//   class Person {
-//     name: string;
-//   }
-//   const david = new Person
-//    console.log(david.name = "Sarka");
-//    console.log("Shiuut");
-//  }, []);
+const greeting = greet("Johnyyyy");
+console.log(greeting); // Output: undefined
+
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+
+let result = addNumbers(3, 4);
+console.log(result); // Output: 7
+
+
+  //  useEffect(() => {
+  //   class Person {
+  //     name: string;
+  //   }
+  //   const david = new Person
+  //    console.log(david.name = "Sarka");
+  //    console.log("Shiuut");
+  //  }, []);
 
   return (
     <div className="App">
-     <h2>Simple Typescript React Tutorial From W3schools</h2>     
+      <h2>Simple Typescript React Tutorial From W3schools</h2>
     </div>
   );
 }
